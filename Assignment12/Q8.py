5c6a662d1653ab478fa7362043c6f736880b0d9c
Manisha Hajapure <manishahajyapure@gmail.com>
2026-08-11 02:13:44 +0530

    Delete Assignment12 directory

--- Assignment12/Q8.py
+++
@@ -1,7 +0,0 @@
-#8. Python Program to Remove the Characters of Odd Index Values in String
-str1=input("enter a string:")
-new_str=" "
-for i in range(len(str1)):
-    if i % 2 ==0:
-        new_str=new_str + str1[i]
-print("string after removing odd index character:",new_str)
